fadd77a4cce4c8de0d514f1648bcba67dcd9e724
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-25 16:02:33 -0300

    Alterando desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,29 +15,44 @@ interface TransactionDTO {
 class TransactionsRepository {
   private transactions: Transaction[];
   private total: number;
+  private sumIncomes: number;
+  private sumOutcomes: number;
+
   constructor() {
     this.transactions = [];
     this.total = 0;
+    this.sumIncomes = 0;
+    this.sumOutcomes = 0;
   }
 
   public all(): Transaction[] {
     return this.transactions;
   }
+
   public getBalance(): Balance {
-    const incomes = this.transactions.filter(transaction => transaction.type === 'income');
-    const outcomes = this.transactions.filter(transaction => transaction.type === 'outcome');
-    const sumIncomes = incomes.map(income => income.value).reduce((acc, atual) => acc + atual);
-    const sumOutcomes = outcomes.map(outcome => outcome.value).reduce((acc, atual) => acc + atual);
-    this.total = sumIncomes - sumOutcomes;
+    const incomesFilter = this.transactions.filter(transaction => transaction.type === 'income')
+      .map(income => income.value);
+    const outcomesFilter = this.transactions.filter(transaction => transaction.type === 'outcome')
+      .map(outcome => outcome.value);
+    if(incomesFilter.length !== 0) {
+      this.sumIncomes = incomesFilter.reduce((acc, atual) => acc + atual);
+    }
+    if(outcomesFilter.length !== 0) {
+      this.sumOutcomes = outcomesFilter.reduce((acc, atual) => acc + atual);
+    }
+    this.total = this.sumIncomes - this.sumOutcomes;
+    if (this.total < 0) {
+      throw Error('Invalid balance');
+    }
     return {
-      income: sumIncomes,
-      outcome: sumOutcomes,
+      income: this.sumIncomes,
+      outcome: this.sumOutcomes,
       total: this.total
     }
   }
 
   public create({title, value, type}: TransactionDTO): Transaction {
-    if (this.total < 0) {
+    if(type == 'outcome' && value > this.sumIncomes) {
       throw Error('Invalid balance');
     }
     const transaction = new Transaction({title, value, type});
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -8,8 +8,8 @@ const createTransactionsService = new CreateTransactionService(transactionsRepos
 
 transactionRouter.get('/', (request, response) => {
   try {
-    const transactions = transactionsRepository.all();
     const balance = transactionsRepository.getBalance();
+    const transactions = transactionsRepository.all();
     return response.status(200).json({transactions : transactions, balance: balance});
   } catch (err) {
     return response.status(400).json({ error: err.message });
@@ -20,6 +20,8 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
     const transaction = createTransactionsService.execute({title, value, type});
+    transactionsRepository.getBalance();
+    //console.log(balance)
     return response.status(201).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
